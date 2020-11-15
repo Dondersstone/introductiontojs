a04638d0b6cbd4d61b863ae87d30add34b33e120
Dondersstone <Christa_van_der_wiel@hotmail.com>
2020-11-16 00:17:47 +0100

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,19 @@
+function upDate(previewPic){
 
+var x=previewPic.src;
+
+var y=previewPic.getAttribute("alt");
+
+document.getElementById("image").style.backgroundImage='url('+x+')';
+
+document.getElementById("image").innerHTML=y;
+
+}
+
+ 
+function unDo(){
+
+    document.getElementById("image").style.backgroundImage='url("")';
+
+    document.getElementById("image").innerHTML='Hover over an image below to display here.';
+}
